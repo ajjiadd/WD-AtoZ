7d484eef74f3c1fbdc5c1193f44c74972fb51fc0
ajjiad <ajjiad.cse@gmail.com>
2025-02-08 12:06:01 +0600

    added practice-module-17

---
+++ practice-module-17/introJavaScript.js
@@ -0,0 +1,26 @@
+console.log("Hello world!");
+
+//variable Declaration
+var price = 250;
+console.log("Price:",price);
+
+//Simple Math
+var totalIhave = 1000;
+var apple = 200;
+var banana = 300;
+var bazar = 200;
+var total = 1000 - (apple+banana+bazar)
+console.log("Total Back Taka:",total);
+
+//avarage Math
+var ban=75, eng=85, mat=88, snce=76;
+var avgMark = (ban+eng+mat+snce)/4;
+console.log("Average Mark:",avgMark);
+
+//Modulus math
+var totalNum = 119;
+var modulus = totalNum % 10;
+console.log("Modulus:",modulus);
+
+
+
